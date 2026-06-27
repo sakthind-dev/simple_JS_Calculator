4023f2f1c865b4be17bc08aa1c35a4a72d07b1f4
sakthind <dsn2603@rediffmail.com>
2026-06-27 14:29:51 +0530

    Calc func extended

---
+++ gitc.bat
@@ -0,0 +1,8 @@
+cd "AI\Simple_JS_Calculator"
+git init
+git status
+git remote -v
+git add.
+git commit -m "Add Python backend and HTML/JS calculator"
+git remote add origin https://github.com/sakthind-dev/Simple_JS_Calculator.git
+git push -u origin main
